e47ec0a6eba7ec2e5f1e3829fc5d771c2a853e70
Alex Vencel <a.vencel@orange.fr>
2023-08-09 10:00:15 -0500

    removed initial nextjs template to start code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,16 @@ module.exports = {
   theme: {
     container: {
       center: true,
-      padding: "2rem",
+      padding: "1rem",
       screens: {
         "2xl": "1400px",
       },
     },
     extend: {
+      fontFamily: {
+        sans: ["Outfit", "sans-serif"],
+        mono: ["JetBrains Mono", "monospace"],
+      },
       colors: {
         border: "hsl(var(--border))",
         input: "hsl(var(--input))",
