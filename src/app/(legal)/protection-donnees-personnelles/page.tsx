9b6715a0187e4dc0fc13ab4f312622c66e17b284
Alex Vencel <a.vencel@orange.fr>
2023-08-10 10:39:53 -0500

    Changed legal text styling

--- src/app/(legal)/protection-donnees-personnelles/page.tsx
+++ src/app/(legal)/protection-donnees-personnelles/page.tsx
@@ -36,14 +36,23 @@ export default function Page() {
         <Balancer>Politique de Protection des Données Personnelles</Balancer>
       </h1>
       <div className="flex flex-col gap-8 max-w-xl">
-        {PRIVACY_CONTENT.map(([title, content]) => (
-          <div key={title}>
-            <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
-              {title}
-            </h2>
-            <p className="text-foreground leading-7 [&:not(:first-child)]:mt-4">
-              {content}
-            </p>
+        {PRIVACY_CONTENT.map(([title, content], index) => (
+          <div key={title} className="flex flex-row gap-6">
+            <div className="flex flex-col gap-4 items-center">
+              <p className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                {index + 1}.
+              </p>
+              <div className="flex-1 w-[1px] bg-border" />
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <h2 className="scroll-m-20 text-2xl font-semibold tracking-tight">
+                {title}
+              </h2>
+              <p className="text-foreground leading-7">
+                <Balancer>{content}</Balancer>
+              </p>
+            </div>
           </div>
         ))}
       </div>
--- src/app/[...slug]/not-found.tsx
+++ src/app/[...slug]/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div className="m-auto flex flex-col item-center justify-center text-center gap-4">
+    <div className="m-auto flex flex-col items-center justify-center text-center gap-4">
       <h1 className="text-5xl font-bold text-primary">404</h1>
       <h1 className="text-3xl font-semibold">Page introuvable</h1>
       <Link href="/" className="mt-10">
