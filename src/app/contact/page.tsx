d2abd6272caa494796495936f47ec0a4cc78ec52
Alex Vencel <a.vencel@orange.fr>
2023-08-11 16:02:53 -0500

    Init contact route

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,11 @@
+import Balancer from "react-wrap-balancer";
+
+export default function Page() {
+  return (
+    <div className="flex flex-col gap-10 w-full items-center">
+      <h1 className="text-4xl font-bold tracking-tight">
+        <Balancer>Contact</Balancer>
+      </h1>
+    </div>
+  );
+}
