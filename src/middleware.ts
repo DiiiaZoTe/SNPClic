53e5ef9055c9bef97f31b25972cfd417a8a3426e
Alex Vencel <a.vencel@orange.fr>
2024-03-10 00:20:47 -0600

    fix: remove log

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,7 +13,7 @@ const SKIP_MIDDLEWARE = [
 ]
 
 export async function middleware(request: NextRequest) {
-  console.log(`${request.nextUrl.pathname} - ${request.headers.get('X-Forwarded-For')}`);
+  console.log(`${request.nextUrl.pathname}`);
 
   // Skip middleware for certain paths
   if (skipMiddleware(request)) return NextResponse.next();
